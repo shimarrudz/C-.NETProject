5c313020d9d98eb60b3a5edcfb37bf7e29b2beb8
Victor Shimada <104167280+shimarrudz@users.noreply.github.com>
2024-02-28 23:03:08 -0300

    Add files via upload

---
+++ C#Project/C#Project/Models/Professional.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Project.Models
+{
+    public class Professional
+    {
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public string Specialty { get; set; }
+        public string CRM { get; set; }
+
+        // Construtor básico
+        public Professional(string name, string cpf)
+        {
+            Name = name;
+            CPF = cpf;
+        }
+
+        // Construtor que chama outro construtor
+        public Professional(string name, string cpf, string specialty, string crm) : this(name, cpf)
+        {
+            Specialty = specialty;
+            CRM = crm;
+        }
+    }
+}
